18f2bf96bcfc7a62bfad2114f97290a4f76435f9
Andrea Shaw <andriafawn@yahoo.com>
2020-11-01 12:35:21 -0500

    feature 3 passing

--- spec/js/DonutMaker.spec.js
+++ spec/js/DonutMaker.spec.js
@@ -3,7 +3,7 @@ describe('FEATURE 1 : Have a way to count donuts.', () => {
     describe('Can add to donut count.', () => {
         let underTest;
         beforeEach(() => {
-            underTest = new DonutMaker;
+            underTest = new DonutMaker();
         });
         it('Should start with a click count of 0.', () => {
             expect(underTest._donutCount).toBe(0);
@@ -11,36 +11,50 @@ describe('FEATURE 1 : Have a way to count donuts.', () => {
         it('Should add one click to the click count when it records a click.', () => {
             underTest.recordClick();
             expect(underTest._donutCount).toBe(1);
-            
-        });
-        it('Should add one click to the click count when it records a click.', () => {
-            underTest.recordClick();
-            underTest.recordClick();
-            expect(underTest._donutCount).toBe(2);
-        });
 
+        });
     });
 });
 
 
-    describe('Feature 2 :Be able to purchase the first Auto Clicker with 100 donuts from your donut Count.', () => {
-        beforeEach(() => {
-            underTest = new DonutMaker;
-            underTest.stashDonutsForTesting();
-        });
-        it('Can retirieve and add to the Auto Clicker count', () => {
-            underTest.recordAutoClick();
-            expect(underTest._donutCount).toBe(1001);
-        });
-        it('Can accumulate total number of donuts', () => {
-            underTest.recordClick();
-            underTest.recordAutoClick();
-            expect(underTest._donutCount).toBe(1002);
-        });
-        it('Subtract the Auto Clicker cost form your donut count', () => {
-            underTest.buyAutoClicker();
-            expect(underTest._donutCount).toBe(900);
-        });
+describe('Feature 2 : Be able to purchase the first Auto Clicker with 100 donuts from your donut Count.', () => {
+    beforeEach(() => {
+        // Arrangement
+        underTest = new DonutMaker();
+        underTest.stashDonutsForTesting();
+    });
+    it('Can retirieve and add to the Auto Clicker count', () => {
+        // Action
+        underTest.buyAutoClicker();
+        // Assertion
+        expect(underTest._autoClickerCount).toBe(1);
+    });
+    // it('Can accumulate total number of donuts', () => {
+    //     underTest.recordClick();
+    //     underTest.recordAutoClick();
+    //     expect(underTest._donutCount).toBe(1002);
+    // });
+    it('Subtracts the Auto Clicker cost from your donut count', () => {
+        underTest.buyAutoClicker();
+        expect(underTest._donutCount).toBe(900);
+    });
+});
+// -----------------------------------------All Tests Passing to Here----------------------------------------------------------
+describe('FEATURE 3 : The cost of each Auto Clicker will go up with each purchase.', () => {
+    beforeEach(() => {
+        underTest = new DonutMaker();
+        underTest.stashDonutsForTesting();
+    });
+    it('Increases the cost of the first Auto Clicker by ten percent', () => {
+        underTest.buyAutoClicker();
+        expect(underTest._autoClickerCost).toBe(110);
+    });
+    it('Increases the cost of an additional Auto Clicker by ten percent', () => {
+        underTest.buyAutoClicker();
+        underTest.buyAutoClicker();
+        expect(underTest._autoClickerCost).toBe(121);
     });
 
+    
 
+});
--- src/js/DonutMaker.js
+++ src/js/DonutMaker.js
@@ -1,23 +1,38 @@
 class DonutMaker {
     constructor() {
         this._donutCount = 0;
-       
+        this._autoClickerCount = 0;
+        this._autoClickerCost = 100;
+
     }
 
     stashDonutsForTesting() {
         this._donutCount += 1000;
     }
-    recordClick(){
+    recordClick() {
         this._donutCount++;
     }
     get donutCount() {
         return this._donutCount;
     }
-    recordAutoClick() {
-        this._donutCount++;
+    // recordAutoClick() {
+    //     this._donutCount++;
+    // }
+    buyAutoClicker() {
+        this._donutCount -= this._autoClickerCost;
+        this._autoClickerCount++;
+        this._autoClickerCost = this._autoClickerCost + (this._autoClickerCost * 0.1);
     }
-    buyAutoClicker(){
-        this._donutCount -= 100;
+    // ---------------All tests passing feature 1 & 2----------------------------------
+    // recordAutoClick() {
+    //     this._autoClickerCount++;
+    // }
+    get autoClickerCount() {
+        return this._autoClickerCount;
     }
+    get autoClickerCost(){
+        return this._autoClickerCost;
+    }
+
 
 }
